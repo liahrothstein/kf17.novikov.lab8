4bd46527b597c83ed34d4a6d3bdafa1ea435b929
liahrothstein <liahrothstein@gmail.com>
2025-12-13 11:13:23 +0300

    fix: exchange pow() function

--- utils/calculate-cube-params.cpp
+++ utils/calculate-cube-params.cpp
@@ -1,8 +1,10 @@
+#include <cmath>
+
 /* Функция вычисления параметров куба
    Назначение: Вычисляет 3 параметра и возвращает их через указатели (по адресу) */
 
 void calculateCubeParams(double a, double *volume, double *surfaceArea, double *faceArea) {
-    *faceArea = a * a;          // Площадь одной грани
+    *faceArea = pow(a, 2);          // Площадь одной грани
     *surfaceArea = 6 * (*faceArea); // Полная площадь (6 граней)
-    *volume = a * a * a;         // Объем
+    *volume = pow(a, 3);         // Объем
 }
